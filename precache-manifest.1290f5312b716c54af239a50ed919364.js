3bef6232d6be260475d8c0b728a81f7c3ae1d894
a-lei-7 <qiyi.li@aol.com>
2020-06-12 17:52:19 +0800

    deploy

---
+++ precache-manifest.1290f5312b716c54af239a50ed919364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af460fcd0c7f2cfb913bc23fcd2dd75",
+    "url": "/money-manager-react-website/index.html"
+  },
+  {
+    "revision": "4f7d48e44ad7b324ed74",
+    "url": "/money-manager-react-website/static/css/main.b12c1e77.chunk.css"
+  },
+  {
+    "revision": "fcc6e0964d06e83a64fa",
+    "url": "/money-manager-react-website/static/js/2.38856d67.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/money-manager-react-website/static/js/2.38856d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7d48e44ad7b324ed74",
+    "url": "/money-manager-react-website/static/js/main.46af4b26.chunk.js"
+  },
+  {
+    "revision": "5593af05375b4b3a6fc5",
+    "url": "/money-manager-react-website/static/js/runtime-main.d398ee9b.js"
+  }
+]);
